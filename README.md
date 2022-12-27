ad30b82ba8299e2436442502fc32f6db238afc50
Kris Hole <kris_hole@hotmail.com>
2022-12-27 02:05:34 +0000

    Update

---
+++ README.md
@@ -0,0 +1,5 @@
+### Auto Runner
+
+- Press G to start running.
+- Press G again or W to stop running.
+- Press CTRL+X to exit application.
--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@
 #include <random>
 #include <string>
 
-constexpr auto VK_KEY_W = 0x11;
-constexpr auto VK_KEY_LSHIFT = 0x2A;
+constexpr auto VSCAN_KEY_W = 0x11;
+constexpr auto VSCAN_KEY_LSHIFT = 0x2A;
 
+constexpr auto VK_KEY_W = 0x57;
 constexpr auto VK_KEY_G = 0x47;
+constexpr auto VK_KEY_X = 0x58;
 
 void KeyEvent(WORD key, bool press) {
 	INPUT ip;
@@ -22,25 +24,43 @@ void KeyEvent(WORD key, bool press) {
 	SendInput(1, &ip, sizeof(INPUT));
 }
 
+static bool moving = false;
+static std::string lastLine = "";
+static std::string nextLine = "Waiting";
+
+void Move(bool val) {
+	moving = val;
+	KeyEvent(VSCAN_KEY_W, moving);
+	KeyEvent(VSCAN_KEY_LSHIFT, moving);
+}
+
 int main() {
-	std::string lastLine = "";
-	std::string nextLine = "Waiting";
+	SetConsoleTitleA("Auto Runner");
+	std::cout << "Pills' Auto Runner" << std::endl;
+	std::cout << "Press G to start running." << std::endl;
+	std::cout << "Press G Again or W to stop running." << std::endl;
+	std::cout << "Press CTRL+X to exit." << std::endl << std::endl;
 
-	bool doOnce = false;
 	bool waitingRelease = false;
-	bool moving = false;
+	bool running = true;
 
-	while (true) {
+	while (running) {
 		if (!waitingRelease && (GetKeyState(VK_KEY_G) & 0x8000)) {
 			waitingRelease = true;
-		
-			moving = !moving;
-			KeyEvent(VK_KEY_W, moving);
-			KeyEvent(VK_KEY_LSHIFT, moving);
-			nextLine = moving ? "Moving" : "Waiting";
+			Move(!moving);
 		} else if (waitingRelease && !(GetKeyState(VK_KEY_G) & 0x8000)) {
 			waitingRelease = false;
+		} else if (moving && !(GetKeyState(VK_KEY_W) & 0x8000)) {
+			Move(false);
 		}
+
+		if ((GetKeyState(VK_LCONTROL) & 0x8000) && (GetKeyState(VK_KEY_X) & 0x8000)) {
+			running = false;
+			Move(false);
+		}
+
+		nextLine = moving ? "Status: Running " : "Status: Waiting  ";
+
 
 		if (lastLine != nextLine) {
 			if (nextLine.length() < lastLine.length()) {
@@ -54,6 +74,5 @@ int main() {
 		}
 		
 		Sleep(16);
-
 	}
 }
